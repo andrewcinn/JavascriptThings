c06658e405d7122005b02b468e337d53d8c6e32c
Ashenyx <eshin2020@chadwickschool.org>
2014-07-19 21:46:57 +0900

    Create .VariableBank변수서버.js

---
+++ .VariableBank변수서버.js
@@ -0,0 +1,157 @@
+
+$(function(){
+
+
+var username = prompt('Who are you? Write your full name. Ex: JohnGreen');
+var userlike = prompt('Who do you love/like? Write his/her full name. Ex: JohnGreen');
+var array = [Chicken, Coke, SallyKang, MikeAhn, BetsyRo, JamesLee, DaekyuKim, RyanKang, KatieKim, WilliamYoon, EricaKim, ChaelinPark, AndyShin];
+
+
+var Chicken = '콜라';
+var Coke = '치킨';
+var SallyKang = "RyanKang";
+var MikeAhn = "KatieKim";
+var BetsyRo = "WilliamYoon";
+var JamesLee = "EricaKim";
+var DaekyuKim = "ChaelinPark";
+var RyanKang = "SallyKang";
+var KatieKim = "MikeAhn";
+var WilliamYoon = "BetsyRo";
+var EricaKim = "JamesLee";
+var ChaelinPark = "DaekyuKim";
+var AndyShin = "EmilyChoi";
+
+if ( userlike == 'SallyKang'){
+
+	if ( SallyKang == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'MikeAhn'){
+
+	if ( MikeAhn == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'BetsyRo'){
+
+	if ( BetsyRo == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'JamesLee'){
+
+	if ( JamesLee == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'DaekyuKim'){
+
+	if ( DaekyuKim == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'RyanKang'){
+
+	if ( RyanKang == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'KatieKim'){
+
+	if ( KatieKim == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'WilliamYoon'){
+
+	if ( WilliamYoon == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'EricaKim'){
+
+	if ( EricaKim == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == 'ChaelinPark'){
+
+	if ( ChaelinPark == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == '치킨' ) {
+
+	if ( Chicken == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+	}
+};
+
+if ( userlike == '콜라' ) {
+
+	if ( Coke == username ) {
+	
+		alert('고백해보세요!');
+	} else {
+	
+		alert('미안 개 다른애 좋아한데');
+};
+
+};
+
+});
